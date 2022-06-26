585a66fd1028674a7447871b29eb0f22f0c3cacf
Abel Kelbessa <akelbessa@haverford.edu>
2022-06-25 23:47:32 -0700

    Initial Commit

---
+++ Parse Chat/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Parse Chat
+//
+//  Created by Abel Kelbessa on 6/25/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Parse Chat/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Parse Chat
+//
+//  Created by Abel Kelbessa on 6/25/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
